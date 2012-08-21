59ec00bb4e167d21642a8c4f20d2a0a290761a08
Jeff Terrace <jterrace@gmail.com>
2012-08-21 18:21:59 -0400

    Added two graphs from priority experiments

--- katasked/bin/priority_experiment_graph.py
+++ katasked/bin/priority_experiment_graph.py
@@ -19,8 +19,10 @@ rc('font', size='18')
 
 def main():
     parser = argparse.ArgumentParser(description='Graphs the results of priority_experiment_runner.py')
-    parser.add_argument('--experiment-dir', '-d', metavar='directory',
+    parser.add_argument('--experiment-dir', '-d', metavar='directory', required=True,
                         help='Directory where experiment was output to')
+    parser.add_argument('--output-dir', '-o', metavar='directory', required=True,
+                        help='Directory where graphs should be output to')
     
     args = parser.parse_args()
 
@@ -28,7 +30,14 @@ def main():
     if os.path.exists(outdir) and not os.path.isdir(outdir):
         parser.error('Invalid directory: %s' % outdir)
     
+    graphdir = os.path.abspath(args.output_dir)
+    if os.path.exists(graphdir) and not os.path.isdir(graphdir):
+        parser.error('Invalid directory: %s' % outdir)
+    if not os.path.exists(graphdir):
+        os.mkdir(graphdir)
+    
     algo_means = collections.defaultdict(list)
+    algo_motion_means = collections.defaultdict(dict)
     
     for priority_algo_name in priority.get_priority_algorithm_names():
         if priority_algo_name == 'FromFile':
@@ -65,6 +74,7 @@ def main():
                 trial_means.append(mean)
                 
             print priority_algo_name, capname, ['%.7g' % v for v in trial_means]
+            algo_motion_means[friendly_name][capname] = trial_means
             algo_means[friendly_name].extend(trial_means)
     
     algo_results = [(numpy.mean(vals), name) for name, vals in algo_means.iteritems()]
@@ -85,54 +95,86 @@ def main():
     print 'As percentage of image size'
     print '==========================='
     for mean, name in sorted(algo_results, reverse=True):
-        print '%s    %.2g %%' % (name.rjust(20), (mean / (1024 * 768)) * 100)
+        print '%s    %s %%' % (name.rjust(20), ('%0.1f' % ((mean / (1024 * 768)) * 100)).rjust(4))
     
-    import sys
-    sys.exit(0)
-    
-    times = collections.defaultdict(list)
-    errvals = collections.defaultdict(list)
-    for exp, _ in dirs:
-        e = os.path.join(ROOT, exp, 'perceptualdiff.json')
-        with open(e, 'r') as f:
-            error_data = json.load(f)
-            for err in error_data:
-                num = float('.'.join(err['filename'].split('.')[:2]))
-                times[exp].append(num)
-                errvals[exp].append(err['perceptualdiff'])
+    positions = numpy.arange(len(algo_results)) + 0.1
+    names = []
+    percentages = []
+    for mean, name in sorted(algo_results):
+        names.append(name)
+        percentages.append((mean / (1024 * 768)) * 100)
     
     fig = plt.figure(figsize=(11.5, 8))
     ax1 = fig.add_subplot(111)
     
-    means = [(numpy.mean(numpy.array(err)), key) for key, err in errvals.iteritems()]
-    means = sorted(means, reverse=True)
-    exporder = [m[1] for m in means]
+    rects = ax1.bar(positions, percentages, color='white', edgecolor='grey', hatch='//')
     
-    colors = iter(['b', 'r', 'g', 'k', 'y', 'b', 'r', 'g'])
-    markers = iter(['h', 's', 'v', 'o', '^', 'v', 'o', '^'])
-    means = {}
-    for exp in exporder:
-        x = numpy.array(times[exp])
-        y = numpy.array(errvals[exp])
-        diffs = numpy.ediff1d(x, to_begin=x[0] - 0)
-        mean = numpy.sum(diffs * y) / x[-1]
-        means[exp] = mean
-        y[y < 1] = 1
-        ax1.plot(x, y, c=next(colors), marker=next(markers), linewidth=2)
+    def autolabel(rects, labels):
+        maxheight = max(rect.get_height() for rect in rects)
+        for rect, label in zip(rects, labels):
+            height = rect.get_height()
+            plt.text(rect.get_x() + rect.get_width() / 2.0, height + maxheight * 0.05, label,
+                    ha='center', va='bottom', rotation=90)
+            plt.text(rect.get_x() + rect.get_width() / 2.0, height - maxheight * 0.07, '%0.1f' % rect.get_height(),
+                    ha='center', va='bottom', clip_on=True)
     
-    #ax1.set_yscale('log')
+    autolabel(rects, names)
     
-    sorted_means = sorted(means.iteritems(), key=lambda e: e[1])
-    dirdict = dict(dirs)
-    for exp, mean in sorted_means:
-        print '%s    %.4g' % (dirdict[exp].rjust(20), mean / max(means.values()))
-    
-    prop = FontProperties(size=12)
-    plt.legend([dirdict[e] for e in exporder], loc=1, prop=prop)
-    plt.ylabel('Perceptual Error (pixels)')
-    plt.xlabel('Time (s)')
+    ax1.get_xaxis().set_visible(False)
+    ax1.yaxis.set_ticks_position('left')
+    plt.yticks(range(0, 101, 10))
+    plt.ylabel('Average perceptual error (percentage of screen size)')
+    plt.ylim((0, 100))
     plt.title('Perceptual Error by Priority Algorithm')
-    plt.savefig('fig.pdf')
+    plt.subplots_adjust(left=0.08, right=0.96, top=0.94, bottom=0.04)
+    plt.savefig(os.path.join(graphdir, 'algo-percentages-bar.pdf'))
+    
+    
+    
+    
+    fig = plt.figure(figsize=(11.5, 8))
+    ax1 = fig.add_subplot(111)
+    
+    positions = numpy.arange(len(algo_results)) + 0.1
+    bar_width = 0.1
+    algo_order = [name for mean, name in sorted(algo_results)]
+    colors = iter(['b', 'r', 'g', 'k', 'y', 'b', 'r', 'g'])
+    
+    cap_means = {}
+    cap_percentages = {}
+    for capname in capdirs:
+        percentages = []
+        for algo_name in algo_order:
+            percentages.append((numpy.mean(algo_motion_means[algo_name][capname]) / (1024 * 768)) * 100)
+        cap_percentages[capname] = percentages
+        cap_means[capname] = numpy.mean(percentages)
+    
+    caps_sorted = sorted((capname, mean) for mean, capname in cap_means.iteritems())
+    caporder = [capname for mean, capname in caps_sorted]
+    
+    rects = []
+    for i, capname in enumerate(caporder):
+        rects.append(ax1.bar(positions + bar_width*i, cap_percentages[capname], bar_width, color=next(colors)))
+    
+    def autolabel2(rects, labels):
+        maxheight = max(rect.get_height() for rect in rects)
+        for rect, label in zip(rects, labels):
+            height = rect.get_height()
+            plt.text(rect.get_x() + rect.get_width() / 2.0, height + maxheight * 0.2, label,
+                    ha='center', va='bottom', rotation=90)
+    
+    autolabel2(rects[1], algo_order)
+    
+    ax1.get_xaxis().set_visible(False)
+    ax1.yaxis.set_ticks_position('left')
+    plt.yticks(range(0, 101, 10))
+    plt.ylabel('Average perceptual error (percentage of screen size)')
+    plt.ylim((0, 100))
+    plt.title('Perceptual Error by Priority Algorithm and Motion Path')
+    prop = FontProperties(size=12)
+    plt.legend([c[8:-5] for c in caporder], loc=2, prop=prop, ncol=7, frameon=False, handlelength=1, columnspacing=1, handletextpad=0.5)
+    plt.subplots_adjust(left=0.08, right=0.96, top=0.94, bottom=0.04)
+    plt.savefig(os.path.join(graphdir, 'algo-percentages-motion-bars.pdf'))
 
 if __name__ == '__main__':
     main()
