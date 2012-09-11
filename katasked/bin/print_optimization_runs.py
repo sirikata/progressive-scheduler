953530cb7ff4daca9158cfa79f3abb4f8739d9b4
Jeff Terrace <jterrace@gmail.com>
2012-09-11 10:44:05 -0400

    Improved optimization iteration printing script

--- katasked/bin/print_optimization_runs.py
+++ katasked/bin/print_optimization_runs.py
@@ -35,11 +35,10 @@ def main():
     
     labels = ['iternum'] + list(inputvars) + ['mean_err']
     MINW = 10
-    min_mean = sys.maxint
-    min_iternum = -1
     print '  '.join(label.rjust(max(len(label),MINW)) for label in labels)
     print '  '.join('=' * max(len(label),MINW) for label in labels)
-    for iternum, iterdata in dirvalues.iteritems():
+    
+    def print_iteration(iternum, iterdata):
         sys.stdout.write(('%d' % iternum).rjust(max(len('iternum'),MINW)))
         sys.stdout.write('  ')
         for label in labels[1:-1]:
@@ -47,13 +46,20 @@ def main():
             sys.stdout.write('  ')
         sys.stdout.write(('%0.2f' % iterdata['mean']).rjust(max(len('mean_err'),MINW)))
         sys.stdout.write('  \n')
-        if iterdata['mean'] < min_mean:
-            min_mean = iterdata['mean']
-            min_iternum = iternum
     
+    vals = []
+    for iternum, iterdata in dirvalues.iteritems():
+        print_iteration(iternum, iterdata)
+        vals.append((iterdata, iternum))
+    
+    vals.sort(key=lambda v: v[0]['mean'])
     print
-    print 'Minimum mean error %.2f at iteration #%d' % (min_mean, min_iternum)
+    print 'Best 5 iterations:'
     print
+    print '  '.join(label.rjust(max(len(label),MINW)) for label in labels)
+    print '  '.join('=' * max(len(label),MINW) for label in labels)
+    for iterdata, iternum in vals[:5]:
+        print_iteration(iternum, iterdata)
 
 if __name__ == '__main__':
     main()
